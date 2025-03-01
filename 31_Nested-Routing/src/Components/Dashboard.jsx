d958ffdb0b2f03cc1afca7acf246b6a4e5df25f3
Dhoni <ankitdhonee@gmail.com>
2025-03-02 00:53:09 +0545

    nested routing

---
+++ 31_Nested-Routing/src/Components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <Outlet/>
+    
+    </div>
+  )
+}
+
+export default Dashboard
