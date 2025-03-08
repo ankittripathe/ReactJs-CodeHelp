0d4cbecafd7e8c4af03d1f31edaf8b03d511d9ae
Dhoni <ankitdhonee@gmail.com>
2025-03-08 23:02:21 +0545

    lifting state

--- 13_State-Lifting/src/App.jsx
+++ 13_State-Lifting/src/App.jsx
@@ -6,13 +6,13 @@ const App = () => {
   // create state
   // manage state
   // change state
-  // sabhi child me state koo sync karwadunga
+  // sabhi child me state koo sync karwa denge
   const [name, setName] = useState("");
 
   return (
     <div>
       <Card title={"Card1"} name={name} setName={setName} />
-      <Card title={"Card2"} name={name} setName={setName} />
+      <Card title="Card2" name={name} setName={setName} />
     </div>
   );
 };
