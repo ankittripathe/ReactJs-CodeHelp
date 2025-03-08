b5aecc7edd158de363396bea99a304808293d087
Dhoni <ankitdhonee@gmail.com>
2025-03-08 14:06:14 +0545

    introduction

--- 01_firstReactApp/src/App.jsx
+++ 01_firstReactApp/src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  let instructor = 'Love Babbar'
+  const instructor = "Love Babbar";
   return (
     <div>
       <h1>My first ReactApp</h1>
-
-      <h2 style={{color: 'red'}}>Code Help</h2>
+      <h2 style={{ color: "red" }}>Code Help</h2>
       <h3>{instructor}</h3>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
