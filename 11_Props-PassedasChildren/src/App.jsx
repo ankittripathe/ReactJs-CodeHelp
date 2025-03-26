dd17f1f1086d34324209a4fc9598200783ae8f6b
Dhoni <ankitdhonee@gmail.com>
2025-03-26 14:59:06 +0545

    props passed as children

--- 11_Props-PassedasChildren/src/App.jsx
+++ 11_Props-PassedasChildren/src/App.jsx
@@ -4,7 +4,9 @@ import Card from "./components/Card";
 const App = () => {
   return (
     <>
-      <div style={{backgroundColor:'crimson', padding:'20px', margin:'20px'}}>
+      <div
+        style={{ backgroundColor: "crimson", padding: "20px", margin: "20px" }}
+      >
         <Card name="Ankit Tripathi">
           <h1>Best Web Dev Course</h1>
           <p>Trying to be Consistant in this</p>
@@ -12,11 +14,15 @@ const App = () => {
         </Card>
       </div>
 
-      <div style={{backgroundColor: 'orange' , padding:'20px', margin:'20px'}}>
+      <div
+        style={{ backgroundColor: "orange", padding: "20px", margin: "20px" }}
+      >
         <Card children="Mein Ek children hu"></Card>
       </div>
 
-      <div style={{backgroundColor: 'green' , padding:'20px', margin:'20px'}}>
+      <div
+        style={{ backgroundColor: "green", padding: "20px", margin: "20px" }}
+      >
         <Card children="Mein Ek children hu">
           Hello Jee, Kaise ho sare // overRight kr dega children ko
         </Card>
