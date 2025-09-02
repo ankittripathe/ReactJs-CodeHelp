098b1285de212747e094cbdebbcd09b3211ec2bf
Ankit Tripathi <ankittripathi.dev@gmail.com>
2025-09-02 15:40:41 +0530

    fragments

--- 03_Fragment-part2/src/App.jsx
+++ 03_Fragment-part2/src/App.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Fragment } from "react";
 
 const App = () => {
   return (
     <Fragment>
       <div>
-        <h1>E-Mart</h1>
+        <h1 style={{color:'yellow', fontSize:'2.5rem'}}>E-Mart</h1>
       </div>
 
       <div>
@@ -13,7 +12,7 @@ const App = () => {
       </div>
 
       <div>
-        <h3>Topic:- Fragment</h3>
+        <h3>Topic: Fragment</h3>
       </div>
     </Fragment>
   );
