18aab3f120622088e93eab8b07484c410d561776
Ankit Tripathi <ankittripathi.dev@gmail.com>
2025-09-06 00:10:04 +0530

    Conditional Rendering

--- 17_Conditional-Rendering/src/App.jsx
+++ 17_Conditional-Rendering/src/App.jsx
@@ -1,41 +1,36 @@
-import React from "react";
 import { useState } from "react";
 import LogoutBtn from "./components/LogoutBtn";
-import LoginBtn from './components/LoginBtn'
+import LoginBtn from "./components/LoginBtn";
 
 const App = () => {
-  // const [isLoggedIn, setLoggedIn] = useState(true);
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(true);
+  // const [isLoggedIn, setLoggedIn] = useState(false);
 
-  //! (1) using if- else statement
+  // (1) Using if-else Statement
   // if (isLoggedIn) {
   //   return <LogoutBtn />;
   // } else {
   //   return <LoginBtn />;
   // }
-  
 
-  //! (2) Using Ternary operator
-  // return (
-  // <div>
-  //   {isLoggedIn ? <LogoutBtn /> : <LoginBtn />}
-  //   </div>
-  // )
+  // (2) Using Ternary Operator
+  // return <div>{isLoggedIn ? <LogoutBtn /> : <LoginBtn />}</div>;
 
-  //! (3) Logical Operator
-  // return (
-  //   <div>
-  //     <h2>Welcome to Web dev Course</h2>
-  //     <div>
-  //       {isLoggedIn && <LogoutBtn/>}
-  //     </div>
-  //   </div>
-  // )
+  // (3) Logical Operator
+  // Renders content only if condition is true
+  return (
+    <div>
+      <h2>Welcome to Web dev Course</h2>
+      <div>
+        {isLoggedIn && <LogoutBtn />}
+      </div>
+    </div>
+  );
 
-  // ! (4) Early Return
-  if (!isLoggedIn) {
-    return <LoginBtn />;
-  }
+  // (4) Early Return
+  // if (!isLoggedIn) {
+  //   return <LoginBtn />;
+  // }
 };
 
 export default App;
