ff64fb1b04f67d89df64a01de5655f1d5f33cf2f
Ankit Tripathi <ankittripathi.dev@gmail.com>
2025-09-05 12:20:28 +0530

    Fragments

--- 02_Fragment/src/App.jsx
+++ 02_Fragment/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  const channel = "Code Help";
+  const YoutubeChannel = "Code Help";
   return (
     <>
       <div>
@@ -9,7 +9,7 @@ const App = () => {
       </div>
 
       <div style={{ color: "green" }}>
-        <h1>{channel}</h1>
+        <h1>{YoutubeChannel}</h1>
       </div>
 
       <div>
