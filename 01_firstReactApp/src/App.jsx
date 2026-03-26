130b5e3740660de040ea61c41a87cb36ebf9a680
ankittripathe <ankittripathi2k24@gmail.com>
2026-03-26 16:40:46 +0530

    react first app

--- 01_firstReactApp/src/App.jsx
+++ 01_firstReactApp/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const instructor = "Love Babbar";
   return (
     <div>
-      <h1 style={{color:"blue"}}>My first ReactApp</h1>
+      <h1 style={{ color: "blue" }}>My first ReactApp</h1>
       <h1 style={{ color: "red" }}>Code Help</h1>
       <h1>{instructor}</h1>
     </div>
