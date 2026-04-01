70ba22bc87a7c596f563c03d140d4cc542a41553
ankittripathe <ankittripathi2k24@gmail.com>
2026-04-02 02:42:59 +0530

    conditional rendering

--- 18_Conditional-Rendering2/src/App.jsx
+++ 18_Conditional-Rendering2/src/App.jsx
@@ -5,13 +5,30 @@ import Notification from "./components/Notification";
 
 const App = () => {
   // const [isLoggedIn, setLoggedIn] = useState(true);
-  const [isLoggedIn, setLoggedIn] = useState(false)
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  // change state when click on button
+  const toogleChange = () => {
+    setLoggedIn((prev) => !prev);
+  };
+
   return (
-    <div>
-      <Greet isLoggedIn={isLoggedIn} />
-      <Greeting isLoggedIn={isLoggedIn} />
-      <Notification isLoggedIn={isLoggedIn} />
-    </div>
+    <>
+      <div className="flex flex-col gap-4">
+        <Greet isLoggedIn={isLoggedIn} />
+        <Greeting isLoggedIn={isLoggedIn} />
+        <Notification isLoggedIn={isLoggedIn} />
+      </div>
+
+      <div className="flex items-center justify-center mt-4">
+        <button
+          className="bg-amber-600 text-black p-2 rounded-md text-xl"
+          onClick={toogleChange}
+        >
+          Toogle Login
+        </button>
+      </div>
+    </>
   );
 };
 
